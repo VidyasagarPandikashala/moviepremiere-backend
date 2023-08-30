785cb4f63aa05110d41d26da40d17a72a8e59da5
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-08-30 12:48:22 +0530

    [Backend][MoviePremiere]main class of application

---
+++ src/main/java/com/moviepremierebackend/MoviePremiereApplication.java
@@ -0,0 +1,13 @@
+package com.moviepremierebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviePremiereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviePremiereApplication.class, args);
+	}
+
+}
