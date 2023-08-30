30f1c17cc15144f2122b25c32acce1c09461b4cb
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-08-30 12:40:32 +0530

    [Backend][User] created userController - created respective endpoints

---
+++ src/main/java/com/moviepremierebackend/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.moviepremierebackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.moviepremierebackend.model.AuthenticationResponse;
+import com.moviepremierebackend.model.Movie;
+import com.moviepremierebackend.model.User;
+import com.moviepremierebackend.service.MovieService;
+import com.moviepremierebackend.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/movie-premiere/v1/")
+@RestController
+public class UserController {
+	@Autowired
+	private UserService userService;
+
+	@PostMapping("/sign-up")
+	public String saveMovieData(@RequestBody User user) {
+		this.userService.saveUser(user);
+		return "User Data saved successfully";
+	}
+
+	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public AuthenticationResponse loginConfirmation(@RequestBody User user) {
+		return this.userService.getAuthentication(user);
+	}
+
+	@PostMapping("/logout")
+	public void setLoggedInandLoggedOutStatus(@RequestParam(name = "id", required = true) int userId) {
+		System.out.println("Logging out . . . ");
+		this.userService.setLoggedInandLoggedOutStatus(userId, false);
+		System.out.println("Logged out . . . " +userId);
+	}
+}
