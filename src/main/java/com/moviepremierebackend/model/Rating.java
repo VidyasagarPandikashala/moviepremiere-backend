878d6d83a12abccf64b6156a94181b600a5f7bc9
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-08-30 12:00:21 +0530

    [Backend] created rating model

---
+++ src/main/java/com/moviepremierebackend/model/Rating.java
@@ -0,0 +1,44 @@
+package com.moviepremierebackend.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@IdClass(RatingId.class)
+@Entity
+public class Rating {
+
+	private int rating;
+
+	@OneToOne
+	@Id
+	private User user;
+
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JsonBackReference
+	@Id
+	private Movie movie;
+
+}
