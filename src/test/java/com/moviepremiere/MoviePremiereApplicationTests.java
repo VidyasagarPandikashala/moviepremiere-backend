4a4373ccba44d8bd6337e1225a0027e356078997
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-08-30 12:49:29 +0530

    [Backend][MoviePremiere]dependencies for applicatn

---
+++ src/test/java/com/moviepremiere/MoviePremiereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviepremiere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviePremiereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
