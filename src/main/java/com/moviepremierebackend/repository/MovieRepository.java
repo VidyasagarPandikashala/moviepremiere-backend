1e86b4183c6692e41c331a4f4902d8a15620c2ca
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-09-16 16:43:03 +0530

    [Movie][Repo[:Added new Query for movie service

--- src/main/java/com/moviepremierebackend/repository/MovieRepository.java
+++ src/main/java/com/moviepremierebackend/repository/MovieRepository.java
@@ -1,5 +1,6 @@
 package com.moviepremierebackend.repository;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,10 +20,17 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 
     @Query("SELECT m FROM Movie m WHERE m.movieName LIKE %:title%")
     ArrayList<Movie> findByMovieLetter(@Param("title") String title);
-    
+
     @Query("SELECT m FROM Movie m WHERE m.movieId = :id")
     Movie findByMovieId(@Param("id") Long id);
- 
-//    @Query("SELECT m FROM Movie m ORDER BY m.rating.rating DESC")
-//    ArrayList<Movie> findByMovieRatingInDescendingOrder();
+
+    @Query("SELECT m FROM Movie m WHERE m.releaseDate >= :currentDate ORDER BY m.releaseDate ASC")
+    List<Movie> findMoviesReleasedAfter(@Param("currentDate") LocalDate currentDate);
+
+    @Query("SELECT m FROM Movie m WHERE m.releaseDate >= :startDate AND m.releaseDate <= :endDate ORDER BY m.releaseDate DESC")
+    List<Movie> findMoviesReleasedLastMonth(@Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate);
+
+    // @Query("SELECT m FROM Movie m ORDER BY m.rating.rating DESC")
+    // ArrayList<Movie> findByMovieRatingInDescendingOrder();
 }
