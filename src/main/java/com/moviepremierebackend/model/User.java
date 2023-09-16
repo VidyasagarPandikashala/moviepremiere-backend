7ba6753633e768d1365c85fff9d9ef0db17615fd
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-09-16 16:51:37 +0530

    [User]:Refactored the code Changed the relationship between both fav and rating

--- src/main/java/com/moviepremierebackend/model/User.java
+++ src/main/java/com/moviepremierebackend/model/User.java
@@ -1,8 +1,5 @@
 package com.moviepremierebackend.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -12,21 +9,21 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.List;
+
 @Data
-@ToString(exclude = {"favourite", "rating"})
+@ToString(exclude = { "favourites", "ratings" })
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class User{
+public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int userId;
@@ -34,13 +31,12 @@ public class User{
 	private String email;
 	private String password;
 	private boolean isLoggedIn;
-	
-	@OneToOne 
-	@JsonIgnore	
-	private Favourite favourite;
-	
-	@OneToOne (mappedBy= "user")
-	@JsonBackReference
-	private Rating rating;
-	
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JsonIgnore
+	private List<Favourite> favourites;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JsonIgnore
+	private List<Rating> ratings;
 }
