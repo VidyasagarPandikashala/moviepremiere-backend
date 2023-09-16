32bd0bcc416687681b47d5e9a17c391c135d494c
VidyasagarPandikasahala <vidya.sagar.six@gmail.com>
2023-09-16 16:48:18 +0530

    [Movie][DTO[:ComingSoonFetch: optimized the search handled the retrieval of the data through JPQuery instead of java code.

--- src/main/java/com/moviepremierebackend/dto/ComingSoonMovieFetcher.java
+++ src/main/java/com/moviepremierebackend/dto/ComingSoonMovieFetcher.java
@@ -2,39 +2,36 @@ package com.moviepremierebackend.dto;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.TimeZone;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 
 import com.moviepremierebackend.model.Movie;
 import com.moviepremierebackend.repository.MovieRepository;
 
 @Component
-public class ComingSoonMovieFetcher implements MovieSectionFetcher{
-	
-	@Autowired
+public class ComingSoonMovieFetcher implements MovieSectionFetcher {
+
 	private final MovieRepository movieRepository;
-	
-	public ComingSoonMovieFetcher (MovieRepository movieRepository) {
+
+	@Autowired
+	public ComingSoonMovieFetcher(MovieRepository movieRepository) {
 		this.movieRepository = movieRepository;
 	}
-	
-	
-	
-	
+
 	@Override
 	public ArrayList<Movie> fetchMovie() {
-		 LocalDate currentDate = LocalDate.now();
-		 System.out.println(currentDate);
-		 ArrayList<Movie> movies = (ArrayList<Movie>) this.movieRepository.findAll();
-		 
-		ArrayList<Movie>filteredMovies =  (ArrayList<Movie>) movies.stream()
-																   .filter(movie->  movie.getReleaseDate().isAfter(currentDate))
-																   .collect(Collectors.toList());
-		System.out.println(filteredMovies);
-			
-			return filteredMovies;
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+		LocalDate currentDate = LocalDate.now();
+
+		System.out.println("Today's Date is........................." + currentDate);
+
+		ArrayList<Movie> filteredMovies = (ArrayList<Movie>) this.movieRepository.findMoviesReleasedAfter(currentDate);
+
+		return filteredMovies;
 	}
 
 }
